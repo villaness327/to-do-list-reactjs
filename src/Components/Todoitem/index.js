668795ea98ba5a4368efd9bdbb543893290a4014
Víctor Illanes <villanes9@gmail.com>
2021-10-27 11:57:09 -0300

    Added comments in TodoItem

--- src/Components/Todoitem/index.js
+++ src/Components/Todoitem/index.js
@@ -7,6 +7,8 @@ function Todoitem(props){
  
 
   //Si se envia algun parametro en la funcion, es necesario envolverlo en una arrow function
+ //El componente <CompleteIcon /> es la funcion que envuelve el componente TodoIcon, y al cual
+ //se le envian las props, y este a su vez se las envia al componente TodoIcon
     return(
             <li className="Todo_Item">
                   
